c8eaaebad36199a3d5b911887ad4e1f3d99ca732
HideakiTouhara <hideee6723@gmail.com>
2018-05-28 20:29:26 +0900

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180526111653) do
+ActiveRecord::Schema.define(version: 20180527091624) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
